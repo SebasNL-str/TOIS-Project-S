1f4a3139ba9b36245cd0c479d9bb979686cdf8af
Sebastian Narváez <jose.narvaez181@std.uni.edu.ni>
2026-05-30 12:56:30 -0600

    Refactor Shader class and update includes

--- Shader.h
+++ Shader.h
@@ -1,111 +1,37 @@
 #pragma once
-#include <glad/glad.h>
-#include <glm.hpp>
+
 #include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <gtc/type_ptr.hpp> //  asegurar compatibilidad con punteros de matrices
 
-class Shader {
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <glm.hpp>
+
+
+
+class Shader
+{
 public:
-    unsigned int ID;
 
-    // Constructor que lee y construye el shader dinámicamente
-    Shader(const char* vertexPath, const char* fragmentPath) {
-        std::string vertexCode;
-        std::string fragmentCode;
-        std::ifstream vShaderFile;
-        std::ifstream fShaderFile;
+    GLuint ID;
 
-        // Configura los objetos ifstream para poder lanzar excepciones
-        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-        try {
-            vShaderFile.open(vertexPath);
-            fShaderFile.open(fragmentPath);
-            std::stringstream vShaderStream, fShaderStream;
+    Shader(
+        const char* vertexPath,
+        const char* fragmentPath
+    );
 
-            vShaderStream << vShaderFile.rdbuf();
-            fShaderStream << fShaderFile.rdbuf();
+    void Use();
 
-            vShaderFile.close();
-            fShaderFile.close();
+    void SetMat4(
+        const std::string& name,
+        glm::mat4 matrix
+    );
 
-            vertexCode = vShaderStream.str();
-            fragmentCode = fShaderStream.str();
-        }
-        catch (std::ifstream::failure& e) {
-            std::cerr << "ERROR::SHADER::ARCHIVO_NO_LEIDO_CORRECTAMENTE: " << e.what() << std::endl;
-        }
-        const char* vShaderCode = vertexCode.c_str();
-        const char* fShaderCode = fragmentCode.c_str();
+    void SetInt(
+        const std::string& name,
+        int value
+    );
 
-        // Compilar los Shaders
-        unsigned int vertex, fragment;
-
-        // Vertex Shader
-        vertex = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertex, 1, &vShaderCode, NULL);
-        glCompileShader(vertex);
-        checkCompileErrors(vertex, "VERTEX");
-
-        // Fragment Shader
-        fragment = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragment, 1, &fShaderCode, NULL);
-        glCompileShader(fragment);
-        checkCompileErrors(fragment, "FRAGMENT");
-
-        // Programa de Shader completo
-        ID = glCreateProgram();
-        glAttachShader(ID, vertex);
-        glAttachShader(ID, fragment);
-        glLinkProgram(ID);
-        checkCompileErrors(ID, "PROGRAM");
-
-        // Limpieza de memoria
-        glDeleteShader(vertex);
-        glDeleteShader(fragment);
-    }
-
-    void use() {
-        glUseProgram(ID);
-    }
-
-    // Funciones de utilidad para interactuar con las variables del GLSL
-    void setBool(const std::string& name, bool value) const {
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
-    }
-    void setInt(const std::string& name, int value) const {
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
-    }
-    void setFloat(const std::string& name, float value) const {
-        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
-    }
-    void setVec3(const std::string& name, const glm::vec3& value) const {
-        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
-    }
-    void setMat4(const std::string& name, const glm::mat4& mat) const {
-        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
-    }
-
-private:
-    void checkCompileErrors(unsigned int shader, std::string type) {
-        int success;
-        char infoLog[1024];
-        if (type != "PROGRAM") {
-            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-            if (!success) {
-                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-                std::cerr << "ERROR::SHADER_COMPILATION_ERROR de tipo: " << type << "\n" << infoLog << "\n" << std::endl;
-            }
-        }
-        else {
-            glGetProgramiv(shader, GL_LINK_STATUS, &success);
-            if (!success) {
-                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-                std::cerr << "ERROR::PROGRAM_LINKING_ERROR de tipo: " << type << "\n" << infoLog << "\n" << std::endl;
-            }
-        }
-    }
+    void SetFloat(const std::string& name, float value);
+    void SetVec3(const std::string& name, const glm::vec3& value);
 };
